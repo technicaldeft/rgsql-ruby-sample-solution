9900d9fb69fe1125900199223665c427c0600e44
Chris Zetter <zetter@users.noreply.github.com>
2025-01-22 16:14:30 +0000

    Chapter 3 - Creating and dropping tables

--- lib/rgsql.rb
+++ lib/rgsql.rb
@@ -2,12 +2,22 @@ require 'rgsql/server'
 require 'rgsql/parser'
 require 'rgsql/runner'
 require 'rgsql/statement'
+require 'rgsql/database'
 
 module RgSql
   class ParsingError < StandardError
   end
 
+  class ValidationError < StandardError
+  end
+
+  def self.run(database, sql)
+    ast = Parser.new(sql).to_ast
+    Runner.new(database, ast).execute
+  end
+
   def self.start_server
-    Server.new.run
+    database = Database.new
+    Server.new(database).run
   end
 end
