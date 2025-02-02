cc6d4fa6196535da0265ed64816b0633521e11dc
Chris Zetter <zetter@users.noreply.github.com>
2025-02-02 15:39:58 +0000

    Chapter 7 - Propagate null
    The simpler, but slightly repetitive, way of doing this would be to add null handling to each operator, or push the operations down to the nodes.
    
    This has similarities to the way 'RETURNS NULL ON NULL INPUT' parameter to 'CREATE FUNCTION' works - if an argument is null, the function isn't executed.

--- lib/rgsql/callable.rb
+++ lib/rgsql/callable.rb
@@ -1,31 +1,57 @@
 module RgSql
-  Callable = Data.define(:name, :input_types, :output_type, :body)
+  Callable = Data.define(:name, :input_types, :output_type, :body, :propagate_null?)
 
   class Callable
     include Nodes
 
+    MINUS_BODY = lambda { |op1, op2 = nil|
+      if op2
+        Int.new(op1.value - op2.value)
+      else
+        Int.new(-op1.value)
+      end
+    }
+
+    AND_BODY = lambda { |op1, op2|
+      result = op1.value && op2.value
+      if result.nil?
+        Null.new
+      else
+        Bool.new(result)
+      end
+    }
+
+    OR_BODY = lambda { |op1, op2|
+      result = op1.value || op2.value
+      if result.nil?
+        Null.new
+      else
+        Bool.new(result)
+      end
+    }
+
     OPERATORS = [
-      new('+', [[Int, Int]], Int,        ->(op1, op2)       { Int.new(op1.value + op2.value) }),
-      new('-', [[Int, Int], [Int]], Int, ->(op1, op2 = nil) { Int.new(op2 ? op1.value - op2.value : -op1.value) }),
-      new('*', [[Int, Int]], Int,        ->(op1, op2)       { Int.new(op1.value * op2.value) }),
-      new('/', [[Int, Int]], Int,        ->(op1, op2)       { Int.new(op1.value / op2.value) }),
+      new('+', [[Int, Int]], Int,        ->(op1, op2) { Int.new(op1.value + op2.value) },       true),
+      new('-', [[Int, Int], [Int]], Int, MINUS_BODY,                                            true),
+      new('*', [[Int, Int]], Int,        ->(op1, op2)       { Int.new(op1.value * op2.value) }, true),
+      new('/', [[Int, Int]], Int,        ->(op1, op2)       { Int.new(op1.value / op2.value) }, true),
 
-      new('NOT', [[Bool]], Bool,       ->(op)       { Bool.new(!op.value) }),
-      new('AND', [[Bool, Bool]], Bool, ->(op1, op2) { Bool.new(op1.value && op2.value) }),
-      new('OR',  [[Bool, Bool]], Bool, ->(op1, op2) { Bool.new(op1.value || op2.value) }),
+      new('NOT', [[Bool]], Bool,        ->(op) { Bool.new(!op.value) }, true),
+      new('AND', [[Bool, Bool]], Bool,  AND_BODY,                       false),
+      new('OR',  [[Bool, Bool]], Bool,  OR_BODY,                        false),
 
-      new('<',  [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i < op2.to_i) }),
-      new('>',  [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i > op2.to_i) }),
-      new('>=', [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i >= op2.to_i) }),
-      new('<=', [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i <= op2.to_i) }),
+      new('<',  [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i < op2.to_i) },  true),
+      new('>',  [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i > op2.to_i) },  true),
+      new('>=', [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i >= op2.to_i) }, true),
+      new('<=', [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.to_i <= op2.to_i) }, true),
 
-      new('=',  [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.value == op2.value) }),
-      new('<>', [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.value != op2.value) })
+      new('=',  [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.value == op2.value) }, true),
+      new('<>', [[Bool, Bool], [Int, Int]], Bool, ->(op1, op2) { Bool.new(op1.value != op2.value) }, true)
     ].freeze
 
     FUNCTIONS = [
-      new('ABS', [[Int]],      Int, ->(arg)        { Int.new(arg.value.abs) }),
-      new('MOD', [[Int, Int]], Int, ->(arg1, arg2) { Int.new(arg1.value % arg2.value) })
+      new('ABS', [[Int]],      Int, ->(arg)        { Int.new(arg.value.abs) }, true),
+      new('MOD', [[Int, Int]], Int, ->(arg1, arg2) { Int.new(arg1.value % arg2.value) }, true)
     ].freeze
 
     def self.find_operator(name)
@@ -39,11 +65,15 @@ module RgSql
     end
 
     def call(inputs)
-      body.call(*inputs)
+      if propagate_null? && inputs.any? { |input| input.is_a?(Null) }
+        Null.new
+      else
+        body.call(*inputs)
+      end
     end
 
     def accepts_types?(types)
-      input_types.include?(types)
+      input_types.any? { |input_type| Types.all_match?(input_type, types) }
     end
   end
 end
