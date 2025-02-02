9ee29b438d407b30fdbe681a7069dd542b6b7a69
Chris Zetter <zetter@users.noreply.github.com>
2025-02-02 15:39:58 +0000

    Chapter 7 - Propagate null
    The simpler, but slightly repetitive, way of doing this would be to add null handling to each operator, or push the operations down to the nodes.
    
    This has similarities to the way 'RETURNS NULL ON NULL INPUT' parameter to 'CREATE FUNCTION' works - if an argument is null, the function isn't executed.

--- lib/rgsql/types.rb
+++ lib/rgsql/types.rb
@@ -1,5 +1,13 @@
 module RgSql
   class Types
+    def self.all_match?(expected_types, actual_types)
+      return false unless expected_types.size == actual_types.size
+
+      expected_types.zip(actual_types).all? do |expected_type, actual_type|
+        match?(expected_type, actual_type)
+      end
+    end
+
     def self.match?(expected_type, actual_type)
       actual_type == Nodes::Null || actual_type == expected_type
     end
