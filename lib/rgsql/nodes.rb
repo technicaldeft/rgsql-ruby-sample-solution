cd2a420229b511732b0471c44be1331a964ad9e5
Chris Zetter <zetter@users.noreply.github.com>
2025-02-12 14:07:16 +0000

    Chapter 8 - Sort with ORDER BY

--- lib/rgsql/nodes.rb
+++ lib/rgsql/nodes.rb
@@ -4,11 +4,27 @@ module RgSql
       def to_i
         value
       end
+
+      def <=>(other)
+        if other.is_a? Null
+          -1
+        else
+          value <=> other.value
+        end
+      end
     end
 
     Bool = Data.define(:value) do
       def to_i
         value ? 1 : 0
+      end
+
+      def <=>(other)
+        if other.is_a? Null
+          -1
+        else
+          to_i <=> other.to_i
+        end
       end
     end
 
@@ -16,10 +32,19 @@ module RgSql
       def value
         nil
       end
+
+      def <=>(other)
+        if other.is_a? Null
+          0
+        else
+          1
+        end
+      end
     end
 
-    Select = Data.define(:select_list, :table, :where)
+    Select = Data.define(:select_list, :table, :where, :order)
     SelectListItem = Data.define(:name, :expression)
+    Order = Data.define(:expression, :ascending)
     CreateTable = Data.define(:table, :columns)
     Column = Data.define(:name, :type)
     DropTable = Data.define(:table, :if_exists)
