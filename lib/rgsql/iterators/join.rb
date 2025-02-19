c7faf1279a80277fe44bc3cd2afeb547d67b0378
Chris Zetter <zetter@users.noreply.github.com>
2025-02-19 15:35:34 +0000

    Chapter 11 - Right joins

--- lib/rgsql/iterators/join.rb
+++ lib/rgsql/iterators/join.rb
@@ -7,6 +7,7 @@ module RgSql
 
         @join = join
         @found_rows = []
+        @matched_right_rows = Set.new
 
         @metadata = metadata
       end
@@ -23,6 +24,8 @@ module RgSql
             return left_row + @right_table.padded_row
           end
         end
+
+        next_unmatched_right_row if keep_unmatched_right_rows?
       end
 
       private
@@ -31,15 +34,36 @@ module RgSql
         @join.type == :left
       end
 
+      def keep_unmatched_right_rows?
+        @join.type == :right
+      end
+
       def next_found_row
         @found_rows.shift
       end
 
+      def next_unmatched_right_row
+        @unmatched_right_rows ||= find_unmatched_right_rows
+        if (right_row = @unmatched_right_rows.shift)
+          null_row = @metadata.row_for_unmatched_right_join(@join.table_alias)
+          null_row + right_row
+        end
+      end
+
       def find_matching_rows(left_row)
-        @right_table.rows.filter_map do |right_row|
+        @right_table.rows.filter_map.with_index do |right_row, index|
           row = left_row + right_row
           result = Expression.evaluate(@join.expression, row, @metadata)
-          row if result == Nodes::Bool.new(true)
+          if result == Nodes::Bool.new(true)
+            @matched_right_rows.add(index)
+            row
+          end
+        end
+      end
+
+      def find_unmatched_right_rows
+        @right_table.rows.filter.with_index do |_right_row, index|
+          !@matched_right_rows.include?(index)
         end
       end
     end
